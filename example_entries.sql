b06728bcb0aafab76ef7f730cce7871ae30675f8
htl14 <htl14@sfu.ca>
2025-03-31 15:55:51 -0700

    Added Volunteer table and made changes to example_entries
    -added a Volunteer table to the database
    -added 10 volunteers to example_entries
    -gave the 10 items actual attributes instead of placeholder names
    -changed the itemID in some of the borrow entries since there were duplicates

--- example_entries.sql
+++ example_entries.sql
@@ -32,16 +32,16 @@ INSERT INTO Room(roomID, roomName, capacity) VALUES (8, 'Study Room 2', 20);
 INSERT INTO Room(roomID, roomName, capacity) VALUES (9, 'Office Room', 54);
 INSERT INTO Room(roomID, roomName, capacity) VALUES (10, 'Board Room', 53);
 -- 10 items
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (1, 'Item Title 1', 'CD', 'Publisher 1', '2005-06-25', 'Author 1', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (2, 'Item Title 2', 'Journal', 'Publisher 2', '2000-03-28', 'Author 2', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (3, 'Item Title 3', 'Magazine', 'Publisher 3', '2001-09-11', 'Author 3', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (4, 'Item Title 4', 'Journal', 'Publisher 4', '2009-12-18', 'Author 4', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (5, 'Item Title 5', 'CD', 'Publisher 5', '2015-11-23', 'Author 5', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (6, 'Item Title 6', 'Book', 'Publisher 6', '2018-10-25', 'Author 6', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (7, 'Item Title 7', 'DVD', 'Publisher 7', '2016-11-30', 'Author 7', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (8, 'Item Title 8', 'DVD', 'Publisher 8', '2022-06-19', 'Author 8', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (9, 'Item Title 9', 'Book', 'Publisher 9', '2022-05-07', 'Author 9', 'Available');
-INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (10, 'Item Title 10', 'CD', 'Publisher 10', '1991-08-22', 'Author 10', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (1, 'Tin Talks', 'CD', 'Calvin Weng', '2005-06-25', 'Tin', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (2, 'Life as a Frog', 'Journal', 'Publisher 2', '2000-03-28', 'Frogadier', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (3, 'Bizarre News at the Castle', 'Magazine', 'Maggy Publishing', '2001-09-11', 'Maggy', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (4, 'Solemn Journey', 'Journal', 'Nova Nexus', '2009-12-18', 'Nolan N', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (5, 'How To Tie a Tie', 'CD', 'Dad Tutorials', '2015-11-23', 'Dad', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (6, "Chef Jo's Cookbook", 'Book', 'Chef Publishing', '2018-10-25', 'Jojo', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (7, 'Mora the Explorer', 'DVD', 'Nora', '2016-11-30', 'Flora', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (8, 'Fast and Angry', 'DVD', 'Anger', '2022-06-19', 'Dominos', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (9, 'LeBron: The Chosen One', 'Book', 'King', '2022-05-07', 'James', 'Available');
+INSERT INTO Item(itemID, title, format, publisher, publishDate, author, status) VALUES (10, 'The Number 10', 'CD', 'Two Hands Publishing', '1991-08-22', 'Greeky', 'Available');
 -- 10 events
 INSERT INTO Event(eventID, title, description, date, startTime, endTime, type, targetAudience, roomID) VALUES (1, 'Clay Mugs', 'Description 1', '2025-04-15', '14:00', '16:00', 'Workshop', 'Senior', 10);
 INSERT INTO Event(eventID, title, description, date, startTime, endTime, type, targetAudience, roomID) VALUES (2, 'Tech Startup', 'Description 2', '2025-11-24', '14:00', '16:00', 'Networking', 'Adult', 4);
@@ -65,13 +65,24 @@ INSERT INTO Attendance(userID, eventID) VALUES (10, 1);
 INSERT INTO Attendance(userID, eventID) VALUES (1, 5);
 INSERT INTO Attendance(userID, eventID) VALUES (7, 6);
 -- 10 instances of borrows
-INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (1, 4, 2, '2023-09-24', '2023-10-08', '2023-10-08', 0.30);
+INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (1, 1, 2, '2023-09-24', '2023-10-08', '2023-10-08', 0.30);
 INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (2, 9, 3, '2024-02-29', '2024-03-14', '2024-03-20', 3.00);
 INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (3, 2, 1, '2023-08-20', '2023-09-03', '2023-09-03', 0.00);
 INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (4, 6, 5, '2024-12-16', '2024-12-30', '2024-12-23', 0.00);
-INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (5, 9, 4, '2023-08-02', '2023-08-16', '2023-08-09', 5.00);
+INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (5, 8, 4, '2023-08-02', '2023-08-16', '2023-08-09', 5.00);
 INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (6, 7, 7, '2023-03-06', '2023-03-20', '2023-03-26', 3.00);
 INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (7, 3, 8, '2024-08-24', '2024-09-07', '2024-09-07', 0.00);
 INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (8, 5, 10, '2023-05-27', '2023-06-10', '2023-06-10', 0.00);
-INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (9, 6, 9, '2024-03-08', '2024-03-22', '2024-03-28', 3.00);
+INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (9, 10, 9, '2024-03-08', '2024-03-22', '2024-03-28', 3.00);
 INSERT INTO Borrow(borrowID, userID, itemID, borrowDate, dueDate, returnDate, fineAmount) VALUES (10, 4, 6, '2023-08-02', '2023-08-16', '2023-08-22', 3.00);
+-- 10 volunteers
+INSERT INTO Volunteer(userID, eventID, role) VALUES (1, 3, 'Guide')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (1, 6, 'Guide')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (1, 9, 'Translator')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (3, 6, 'Leader')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (6, 3, 'Guide')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (7, 1, 'Helper')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (4, 2, 'Leader')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (10, 8, 'Translator')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (9, 6, 'Guide')
+INSERT INTO Volunteer(userID, eventID, role) VALUES (9, 4, 'Translator')
---
+++ volunteer.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+def getConnection():
+    return sqlite3.connect("library.db")
+
+def volunteerForEvent():
+    conn = getConnection()
+    cursor = conn.cursor()
+
+    # Asks the user for the necessary information to create a Volunteer entry
+    userID = input("Enter your User ID: ").strip()
+    eventID = input("Enter your Event ID: ").strip()
+    role = input("Enter your role (Helper, Leader, Guide, Translator, etc): ").strip()
+
+    # Check if user is already volunteering for this event
+    cursor.execute("SELECT * FROM Volunteer WHERE userID = ? AND eventID = ?", (userID, eventID))
+    if cursor.fetchone():
+        print("You are already registered as a volunteer for this event")
+        conn.close()
+        return
+    
+    cursor.execute(
+            "INSERT INTO Volunteer (userID, eventID, role) VALUES (?, ?, ?)",
+            (userID, eventID, role))
+    conn.commit()
+    cursor.execute("SELECT title FROM Event WHERE eventID = ?", (eventID,))
+    event = cursor.fetchone()
+    eventName = event[0]
+    cursor.execute("SELECT firstName, lastName FROM User WHERE userID = ?", (userID,))
+    user = cursor.fetchone()
+    userFirstName, userLastName = user
+
+    print(f"Successfully registered {userFirstName} {userLastName} as a volunteer ({role}) for the event, {eventName}")
